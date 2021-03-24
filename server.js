ad951822a970780850c63097bafe0e73693735f4
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-03-24 15:28:44 -0400

    style: change to arrow funcions

--- server.js
+++ server.js
@@ -10,14 +10,20 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+// GET route for the shorturl
+app.get('/api/shorturl/id',(req, res) =>{
+  res.json({ greeting: 'hello changed to shorturl this will redirect to original url' });
 });
+
+//POST route for new shortURL
+
+app.post('api/shorturl/new', (req,res)=>{
+	console.log("Hello from the POST route")
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
